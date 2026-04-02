c51b06bd09c6c1473482f0298d79de70e2ec67d0
Aleksandr Borisov <borisov@clickadu.com>
2026-04-02 16:35:25 +0300

    fix conflicts

--- mysqld_exporter.go
+++ mysqld_exporter.go
@@ -271,19 +271,18 @@ func main() {
 		}
 	}
 
-	var dsn string
-	ctx := context.Background()
-
-	cfg := c.GetConfig()
-	cfgsection, ok := cfg.Sections["client"]
-	if !ok {
-		logger.Error("Failed to parse section [client] from config file", "err", err)
-	}
-	if dsn, err = cfgsection.FormDSN(""); err != nil {
-		logger.Error("Failed to form dsn from section [client]", "err", err)
-	}
-	
 	if collector.ShouldWriteHeartbeat() {
+		var dsn string
+		ctx := context.Background()
+
+		cfg := c.GetConfig()
+		cfgsection, ok := cfg.Sections["client"]
+		if !ok {
+			logger.Error("Failed to parse section [client] from config file", "err", err)
+		}
+		if dsn, err = cfgsection.FormDSN(""); err != nil {
+			logger.Error("Failed to form dsn from section [client]", "err", err)
+		}
 		hbw, err := collector.NewHeartbeatWriter(ctx, logger, dsn)
 		if err != nil {
 			logger.Error("failed to initialize heartbeat writer", "err", err)
